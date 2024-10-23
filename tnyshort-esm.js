eedec1bad38828a3339fb1967aca944e481eb20e
Kaze aja <michaelagam03@gmail.com>
2024-10-23 19:12:34 +0800

    Create tnyshort-esm.js

---
+++ tnyshort-esm.js
@@ -0,0 +1,14 @@
+// https://whatsapp.com/channel/0029VaW25g5F1YlKczMRmd1h/1224
+export async function tnyim(url) {
+        try {
+            const response = await fetch(`https://tny.im/yourls-api.php?format=json&action=shorturl&url=${url}`);
+            const result = await response.json();
+            return result.shorturl;
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+
+//Contoh Penggunaan return tnyim('https://tanakasenn.com')
